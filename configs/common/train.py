2a6d9525e8e540ca8871ce81de94699d8509c587
gklpcsgn <gklpcsgn94@gmail.com>
2024-03-31 10:29:09 +0300

    little hint for detectron

--- configs/common/train.py
+++ configs/common/train.py
@@ -1,9 +1,9 @@
-# Common training-related configs that are designed for "tools/lazyconfig_train_net.py"
-# You can use your own instead, together with your own train_net.py
+# if you dont know the epoch logic 
+# https://github.com/facebookresearch/detectron2/discussions/3622
 train = dict(
     output_dir="./output",
     init_checkpoint="",
-    max_iter=90000,
+    max_iter=100,
     amp=dict(enabled=False),  # options for Automatic Mixed Precision
     ddp=dict(  # options for DistributedDataParallel
         broadcast_buffers=False,
@@ -11,7 +11,7 @@ train = dict(
         fp16_compression=False,
     ),
     checkpointer=dict(period=5000, max_to_keep=100),  # options for PeriodicCheckpointer
-    eval_period=5000,
+    eval_period=10,
     log_period=20,
     device="cuda"
     # ...
